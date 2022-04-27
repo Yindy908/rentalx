e5e7a602c538556c348c0154437aa6fb8a668454
yindy908 <tiago.messias7767@gmail.com>
2022-04-27 20:51:00 -0300

    minor changes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -9,11 +9,15 @@ const categories: Category[] = [];
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body;
 
-    const category: Category = {
+    const category = new Category();
+    // category.name = name;
+    // category.description = description;
+    // category.created_at = new Date();
+    Object.assign(category, {
         name,
         description,
         created_at: new Date(),
-    };
+    });
 
     categories.push(category);
     return response.status(201).json({ category });
